9a3ec29c4fa8bd8c2099fe044076bbbfaae18f6e
Tinia Liu <liutianyipku@gmail.com>
2017-11-12 23:05:37 -0500

    No adding new points

--- app.js
+++ app.js
@@ -256,13 +256,13 @@ function mousedown() {
   if(d3.event.ctrlKey || mousedown_node || mousedown_link) return;
 
   // insert new node at point
-  var point = d3.mouse(this),
-      node = {id: ++lastNodeId, reflexive: false};
-  node.x = point[0];
-  node.y = point[1];
-  nodes.push(node);
+  //var point = d3.mouse(this),
+    //  node = {id: ++lastNodeId, reflexive: false};
+  //node.x = point[0];
+  //node.y = point[1];
+  //nodes.push(node);
 
-  restart();
+  //restart();
 }
 
 function mousemove() {
